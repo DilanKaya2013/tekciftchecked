fdc3bf8a4ad555e36a81604b8986cb590b6bacef
DilanKaya <145452308+DilanKaya2013@users.noreply.github.com>
2023-10-31 13:32:03 +0300

    Add files via upload

---
+++ ForDonguTekCiftCheckBox-main/WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chkTek = new System.Windows.Forms.CheckBox();
+            this.chkCift = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtToplam = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSayi1.Location = new System.Drawing.Point(43, 140);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(36, 29);
+            this.txtSayi1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(94, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ile";
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSayi2.Location = new System.Drawing.Point(129, 140);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(36, 29);
+            this.txtSayi2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(180, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "arasındaki";
+            // 
+            // chkTek
+            // 
+            this.chkTek.AutoSize = true;
+            this.chkTek.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.chkTek.Location = new System.Drawing.Point(289, 121);
+            this.chkTek.Name = "chkTek";
+            this.chkTek.Size = new System.Drawing.Size(136, 28);
+            this.chkTek.TabIndex = 4;
+            this.chkTek.Text = "Tek Sayıların";
+            this.chkTek.UseVisualStyleBackColor = true;
+            // 
+            // chkCift
+            // 
+            this.chkCift.AutoSize = true;
+            this.chkCift.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.chkCift.Location = new System.Drawing.Point(289, 155);
+            this.chkCift.Name = "chkCift";
+            this.chkCift.Size = new System.Drawing.Size(129, 28);
+            this.chkCift.TabIndex = 5;
+            this.chkCift.Text = "Çift Sayıların";
+            this.chkCift.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(441, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "toplamı =";
+            // 
+            // txtToplam
+            // 
+            this.txtToplam.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtToplam.Location = new System.Drawing.Point(533, 137);
+            this.txtToplam.Name = "txtToplam";
+            this.txtToplam.Size = new System.Drawing.Size(60, 29);
+            this.txtToplam.TabIndex = 7;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(644, 143);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(75, 23);
+            this.btnHesapla.TabIndex = 8;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 434);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.txtToplam);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chkCift);
+            this.Controls.Add(this.chkTek);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSayi1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSayi2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox chkTek;
+        private System.Windows.Forms.CheckBox chkCift;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtToplam;
+        private System.Windows.Forms.Button btnHesapla;
+    }
+}
+
---
+++ ForDonguTekCiftCheckBox-main/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            int toplam = 0;
+
+
+            int sayi1 = Convert.ToInt32(txtSayi1.Text);
+            int sayi2 = Convert.ToInt32(txtSayi2.Text);
+
+
+            for (int i = sayi1;i<=sayi2;i++)
+            {
+                if (chkTek.Checked==true && chkCift.Checked==false)
+                {
+                    if (i % 2 == 1)
+                    {
+                        toplam += i;
+                    }
+                }
+                else if (chkCift.Checked==true && chkTek.Checked==false) 
+                {
+                    if (i % 2 == 0)
+                    {
+                        toplam += i;
+                    }
+                }
+                else if(chkTek.Checked==true &&chkCift.Checked==true) 
+                {
+                    toplam += i;
+                }
+                else
+                {
+                    toplam = 0;
+                }
+
+            }
+
+            txtToplam.Text = toplam.ToString();
+
+
+
+        }
+    }
+}
